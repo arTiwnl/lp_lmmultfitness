3f4fad51bf63d8a5c90214159c0ea5d7f033366b
Arthur Carvalho <57073690+arTiwnl@users.noreply.github.com>
2024-08-28 15:08:07 -0300

    mx-auto

--- src/components/GymPass.jsx
+++ src/components/GymPass.jsx
@@ -6,7 +6,7 @@ export default function GymPass (){
 
       
 
-<section className="bg-yellow-400 text-white">
+<section className="mx-auto bg-yellow-400 text-white">
   <div className="max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-40 lg:py-8">
     <div className="grid grid-cols-1 gap-y-8 lg:grid-cols-2 lg:items-center lg:gap-x-16">
       <div className="mx-auto max-w-lg ltr:sm:text-center lg:mx-0 ltr:lg:text-left rtl:lg:text-right">
