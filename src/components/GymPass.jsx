9240dfe4673d74c246fc14b486370b004078f276
Arthur Carvalho <57073690+arTiwnl@users.noreply.github.com>
2024-08-28 15:34:29 -0300

    feat: color

--- src/components/GymPass.jsx
+++ src/components/GymPass.jsx
@@ -10,7 +10,7 @@ export default function GymPass (){
   <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-40 lg:py-8">
     <div className="grid grid-cols-1 gap-y-8 lg:grid-cols-2 lg:items-center lg:gap-x-16">
       <div className="mx-auto max-w-lg ltr:sm:text-center lg:mx-0 ltr:lg:text-left rtl:lg:text-right">
-        <h2 className="text-3xl font-bold sm:text-4xl">Aceitamos Gympass e Totalpass</h2>
+        <h2 className=" text-gray-900 text-3xl font-bold sm:text-4xl">Aceitamos Gympass e Totalpass</h2>
 
        
 
