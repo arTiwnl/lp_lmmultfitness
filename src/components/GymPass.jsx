418d63d7f537d7336426acb68e4f8994e0da9463
artiwnl <contatocarvalhoarthur@gmail.com>
2024-08-29 23:38:27 -0300

    att

--- src/components/GymPass.jsx
+++ src/components/GymPass.jsx
@@ -9,9 +9,9 @@ export default function GymPass (){
 
 <section className=" bg-yellow-400 text-white">
   <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-40 lg:py-8">
-    <div className="grid grid-cols-1 gap-y-8 lg:grid-cols-2 lg:items-center lg:gap-x-16">
+    <div className="grid grid-cols-1 gap-y-8 lg:grid-cols-2 lg:items-center lg:gap-x-">
       <div className="mx-auto max-w-lg ltr:sm:text-center lg:mx-0 ltr:lg:text-left rtl:lg:text-right">
-        <h2 className=" text-gray-900 text-3xl font-bold sm:text-4xl">Aceitamos Gympass e Totalpass</h2>
+        <h2 className=" text-gray-900 text-3xl font-bold sm:text-4xl">Somos Parceiros</h2>
 
        
 
