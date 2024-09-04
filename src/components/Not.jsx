c7d02d32f848a2153a92b4eb3d562157c3994ca9
Arthur Carvalho <contatocarvalhoarthur@gmail.com>
2024-09-04 20:19:13 -0300

    feat:fixed-not-top

--- src/components/Not.jsx
+++ src/components/Not.jsx
@@ -1,7 +1,7 @@
 export default function Not (){
 
     return (
-    <div className="bg-yellow-400 px-4 py-3 text-black">
+    <div className="z-50 w-full	position: fixed bg-yellow-400 px-4 py-3 text-black">
   <p className="text-center text-sm font-medium">
     Quer Conhecer Nossa Unidade? <a href="#" className="inline-block underline"> Clique Aqui!</a>
   </p>
